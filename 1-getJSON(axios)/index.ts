1305de72f20b74fb1434adb9963041177afd0116
Sergey87 <serge.techno@gmail.com>
2022-10-05 00:15:00 +0300

    added interface and error detection with typescript

--- 1-getJSON(axios)/index.ts
+++ 1-getJSON(axios)/index.ts
@@ -2,10 +2,31 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 // http get request  - trying to extract data
 axios.get(url).then((response) => {
-  console.log(response.data);
+  // response.data has properties of: id title completed
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The todo with ID: ${id}
+  Has a title of: ${title}
+  Is it finished? ${completed}
+  `);
+};
 
 // tsc index.ts
 
