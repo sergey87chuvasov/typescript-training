dcb92a668c0b62a1360b15dd3505c1542d37d5d0
Sergey87 <serge.techno@gmail.com>
2022-10-18 01:24:24 +0300

    display google maps in browser

--- maps-project/src/index.ts
+++ maps-project/src/index.ts
@@ -1,10 +1,59 @@
-import { User } from './User';
-import { Company } from './Company';
+// import '@types/googlemaps';
+// import '@types/google.maps';
+// import '@types/google-maps';
+import { Loader } from '@googlemaps/js-api-loader';
 
-const user = new User();
-console.log(user);
+// import { User } from './User';
+// import { Company } from './Company';
 
-const company = new Company();
-console.log(company);
+// const user = new User();
+// console.log(user);
 
-google;
+// const company = new Company();
+// console.log(company);
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
+
+// opt 1
+// let map: google.maps.Map;
+
+// function initMap(): void {
+//   map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// declare global {
+//   interface Window {
+//     initMap: () => void;
+//   }
+// }
+// window.initMap = initMap;
+
+// opt 2
+
+// let map: google.maps.Map;
+
+// const additionalOptions = {};
+
+// const loader = new Loader({
+//   apiKey: 'AIzaSyBCfaym8xxhYxLlCLWckS3r937JZIT7cqA',
+//   version: 'weekly',
+//   ...additionalOptions,
+// });
+
+// loader.load().then(() => {
+//   map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 1,
+//   });
+// });
+
+// export {};
