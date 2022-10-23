12736231a3dc2086834de054cf07f6dc7c42994c
Sergey87 <serge.techno@gmail.com>
2022-10-23 21:54:32 +0300

    added class Sort

--- sort-project/src/index.ts
+++ sort-project/src/index.ts
@@ -1,3 +1,29 @@
-console.log('hello');
+class Sorter {
+  // 1 option
+  // collection: number[];
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // }
 
-const test = 'hello';
+  // 2 option - the same code
+  constructor(public collection: number[]) {}
+  //method - bublle
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          // схема обмена значениями
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([4, -3, 11, 2]);
+sorter.sort();
+console.log(sorter.collection); // [ -3, 2, 4, 11 ]
