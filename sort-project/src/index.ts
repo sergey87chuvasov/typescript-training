e56d2bdfc867696be61f3b18087f71408c3e7887
Sergey87 <serge.techno@gmail.com>
2022-10-24 22:52:24 +0300

    added guard of types

--- sort-project/src/index.ts
+++ sort-project/src/index.ts
@@ -6,18 +6,28 @@ class Sorter {
   // }
 
   // 2 option - the same code
-  constructor(public collection: number[]) {}
+  // common collection
+  constructor(public collection: number[] | string) {}
   //method - bublle
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          // схема обмена значениями
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        // guard of types
+        if (this.collection instanceof Array) {
+          // this.collection. - all properties [number] - has type array
+          // if coolect for arr of numbers
+          if (this.collection[j] > this.collection[j + 1]) {
+            // схема обмена значениями только для numbers а не для string
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
+        }
+        // if collect is string
+        if (typeof this.collection === 'string') {
+          // this.collection. // all properties to string
         }
       }
     }
