942fd4e5043c7ee8c43d1bb4e1a94eea9381067b
Sergey87 <serge.techno@gmail.com>
2022-10-27 23:44:59 +0300

    added CharacterCollection.ts

--- sort-project/src/index.ts
+++ sort-project/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([4, -3, 111, 2]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data); // [ -3, 2, 4, 111 ]
+
+const charactersCollection = new CharactersCollection('ZxYmba');
+const sorter1 = new Sorter(charactersCollection);
+sorter1.sort();
+console.log(charactersCollection.data); // abmxYZ
