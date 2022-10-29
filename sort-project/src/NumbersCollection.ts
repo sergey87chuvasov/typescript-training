62b360d62f3318400c46916caf039741be2b2bed
Sergey87 <serge.techno@gmail.com>
2022-10-29 18:46:38 +0300

    added abstract classes

--- sort-project/src/NumbersCollection.ts
+++ sort-project/src/NumbersCollection.ts
@@ -1,13 +1,9 @@
-// export class NumbersCollection {
-//   data: number[];
+import { Sorter } from './Sorter';
 
-//   constructor(data: number[]) {
-//     this.data = data;
-//   }
-// }
-
-export class NumbersCollection {
-  constructor(public data: number[]) {}
+export class NumbersCollection extends Sorter {
+  constructor(public data: number[]) {
+    super();
+  }
 
   get length(): number {
     return this.data.length;
--- sort-project/src/index.ts
+++ sort-project/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([4, -3, 111, 2]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,18 @@ const charactersCollection = new CharactersCollection('ZxYmba');
 const sorter1 = new Sorter(charactersCollection);
 sorter1.sort();
 console.log(charactersCollection.data); // abmxYZ
+
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(-11);
+linkedList.add(-3);
+linkedList.add(7);
+const sorter2 = new Sorter(linkedList);
+sorter2.sort();
+linkedList.print();
+/*
+[start:*run] -11
+[start:*run] -3
+[start:*run] 7
+[start:*run] 100
+ */
