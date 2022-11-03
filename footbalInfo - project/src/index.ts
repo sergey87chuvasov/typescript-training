e4e97ad98935ac5d54379f426eef6096cf40f294
Sergey87 <serge.techno@gmail.com>
2022-11-04 00:50:10 +0300

    added Enums in project

--- footbalInfo - project/src/index.ts
+++ footbalInfo - project/src/index.ts
@@ -12,13 +12,37 @@ const matches = fs
 
 // console.log(matches);
 
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
+
+// with big LETTER - Enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+// MatchResult.AwayWin === 'A'
+
+// not need for us - only sample
+// const getMatchResult = (match: string[]): MatchResult => {
+//   if (match[5] === 'H') {
+//     return MatchResult.HomeWin;
+//   } else if (match[5] === 'A') {
+//     return MatchResult.AwayWin;
+//   }
+
+//   return MatchResult.Draw;
+// };
+
 let manUnitedWins = 0;
 
 // match -отдельная запись о матче - всего 0-1-2-3-4-5-6 индексов данных
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
