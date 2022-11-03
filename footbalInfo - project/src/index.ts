6ab829c76bafce65063613326a5a878aa4ccad13
Sergey87 <serge.techno@gmail.com>
2022-11-03 23:49:00 +0300

    added reading csv file and its data processing

--- footbalInfo - project/src/index.ts
+++ footbalInfo - project/src/index.ts
@@ -1,1 +1,26 @@
-console.log('test');
+import fs from 'fs';
+
+// в переменн будут содержать все файлы csv в одной строке
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+// console.log(matches);
+
+let manUnitedWins = 0;
+
+// match -отдельная запись о матче - всего 0-1-2-3-4-5-6 индексов данных
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man Unnited won ${manUnitedWins} times `); // Man Unnited won 18 times
